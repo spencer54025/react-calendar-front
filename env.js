a7df591125d7684e2784319cff76a86f5d0b2946
spencer54025 <spencervp54025@gmail.com>
2022-05-03 13:55:29 -0600

    fixed fetch call

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 module.exports = {
   devServer: {
     host: 'localhost',
-    port: 5000
+    port: 4000
   }
 };
--- src/components/calendar.js
+++ src/components/calendar.js
@@ -7,13 +7,61 @@ import Footer from './footer'
 export default class Calender extends Component {
     constructor(props){
         super(props)
+
+        this.monthList = [
+			"January",
+			"February",
+			"March",
+			"April",
+			"May",
+			"June",
+			"July",
+			"August",
+			"September",
+			"October",
+			"November",
+			"December",
+		];
+
+        this.now = this.calculateDateData()
+
+        this.state = {
+            month: {},
+            monthData: []
+        }
+    }
+
+    calculateDateData() {
+        const now = new Date()
+        const month = this.monthList[now.getMonth()]
+        const year = now.getFullYear()
+        return { month, year }
+    }
+
+    componentDidMount() {
+        fetch('http://127.0.0.1:5000/months')
+            .then((response) => response.json())
+            .then((data) => {
+                console.log("API Data:", data),
+                    this.setState({
+                        monthData: data,
+                        month: data.filter(
+                            (month) =>
+                                month.name === this.now.month &&
+                                month.year === this.now.year
+                        )[0],
+                    });
+            });
     }
 
     render(){
-    return(
-        <div>
-            
-        </div>
-    )
+        return(
+            <div className='calendar-container'>
+                <h2>calendar by spencer</h2>
+                <Header monthName={this.state.month.name} />
+                <Weeks month={this.state.month} />
+                <Footer year={this.state.month.year} />
+            </div>
+        )
     }
 }
