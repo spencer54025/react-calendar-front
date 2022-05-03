3f378e5140d9ad0ef9b0b66a39e20c5678a2b08c
spencer54025 <spencervp54025@gmail.com>
2022-05-03 16:19:22 -0600

    added fetch calls for post and put

--- src/components/days-title.js
+++ src/components/days-title.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-export default function DayTitle(props) {
+export default function DayTitle() {
     return (
-        <div>
-            <h4>Days</h4>
+        <div className='day-title-wrapper'>
+            <div className="day-name">Sunday</div>
+            <div className="day-name">Monday</div>
+            <div className="day-name">Tuesday</div>
+            <div className="day-name">Wednesday</div>
+            <div className="day-name">Thursday</div>
+            <div className="day-name">Friday</div>
+            <div className="day-name">Saturday</div>
         </div>
     )
 }
--- src/components/dayswrapper.js
+++ src/components/dayswrapper.js
@@ -3,12 +3,61 @@ import React, { Component } from 'react'
 export default class DaysWrapper extends Component{
     constructor(props){
         super(props)
+
+        const reminder = props.month ? props.month.reminders.filter(reminders => reminders.date === props.date)[0] : null
+
+        this.state = {
+            textInput: '',
+            reminderExists: reminder ? reminder.text : ''
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleSubmit() {
+        if (this.state.reminderExists && this.state.textInput !== '') {
+            fetch('http://127.0.0.1:5000/reminder/add', {
+                method: "POST",
+                headers: { "content-type": "application/json" },
+                body: JSON.stringify({
+                    text: this.state.textInput,
+                    date: this.props.date,
+                    month_id: this.props.month.id
+                })
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (typeof data === "string") {
+                    console.log(data)
+                }else {
+                    this.setState({
+                        reminderExists: true
+                    })
+                }
+            })
+            .catch(error => console.log('an error occured', error))
+        } else if(this.state.reminderExists && this.state.textInput !== '') {
+            fetch(`http://127.0.0.1:5000/reminder/update/${this.props.month.id}/${this.props.month.date}`, {
+                method: "PUT",
+                headers: { "content-type": "application/json" },
+                body: JSON.stringify({
+                    text: this.state.textInput,
+                })
+            })
+        }
     }
 
     render(){
         return(
-            <div>
-                <span>days</span>
+            <div className={this.props.overflow ? "day-overflow" : "day"}>
+                <span className='date'>{this.props.date}</span>
+                <textarea 
+                className="reminderField"
+                disabled={this.props.overflow}
+                value={this.state.textInput}
+                onChange={(event => 
+                    this.setState({textInput: event.target.value})).bind(this)}
+                ></textarea>
             </div>
         )
     }
